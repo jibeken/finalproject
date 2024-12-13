a984b535f4272e1c5c6164318b12a9d4cb639dbd
jibeken <kenjibek1606@icloud.com>
2024-12-13 12:04:38 +0600

    first commit

---
+++ math-site/src/App.js
@@ -0,0 +1,14 @@
+import { BottomNavigation } from '@mui/material';
+import './App.css';
+import DrawerAppBar from './components/DrawerAppBar';
+
+function App() {
+  return (
+    <div>
+      <DrawerAppBar/>
+      <BottomNavigation/>
+    </div>
+  );
+}
+
+export default App;
